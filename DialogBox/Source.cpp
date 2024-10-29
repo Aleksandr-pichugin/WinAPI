5ab7599fb6aee7a0c602dc941bb3c635d1c12a56
Александр Пичугин <aleksandrpicugin700@gmail.com>
2024-10-29 20:58:10 +0300

    Copy DONE

--- DialogBox/Source.cpp
+++ DialogBox/Source.cpp
@@ -21,6 +21,18 @@ BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 	case WM_COMMAND:
 		switch (LOWORD(wParam))
 		{
+		case IDC_BUTTON_COPY:
+		{
+			HWND hEditLogin = GetDlgItem(hwnd, IDC_EDIT_LOGIN);
+			HWND hEditPassword = GetDlgItem(hwnd, IDC_EDIT_PASSWORD);
+			//функция GetDlgItem()возвращает HWND дочернего элемента окна по его 
+			//HWND - HANDLER to Window (Дескриптор окна ) - это число , при помощи которого можно обратиться к окну
+			CONST INT SIZE = 256;
+			CHAR sz_buffer[SIZE] = {};
+			SendMessage(hEditLogin, WM_GETTEXT, SIZE, (LPARAM)sz_buffer);
+			SendMessage(hEditPassword, WM_SETTEXT, 0, (LPARAM)sz_buffer);
+		}
+			break;
 		case IDOK: MessageBox(hwnd, "Была нажата кнопка ОК", "Info", MB_OK | MB_ICONINFORMATION); break;
 		case IDCANCEL: EndDialog(hwnd, 0);
 		}
