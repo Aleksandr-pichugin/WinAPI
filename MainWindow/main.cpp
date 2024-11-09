7245ed7ad5161e3d04ee3c253ee4728ca5896065
Александр Пичугин <aleksandrpicugin700@gmail.com>
2024-11-09 20:17:37 +0300

    Window Size & Position DONE

--- MainWindow/main.cpp
+++ MainWindow/main.cpp
@@ -1,5 +1,6 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include<windows.h>
-
+#include<cstdio>
 
 CONST CHAR g_sz_MY_WINDOW_CLASS[] = "My Window";
 
@@ -34,14 +35,24 @@ INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, IN
 	}
 
 	//2)Создание окна :
+
+	INT screen_width = GetSystemMetrics(SM_CXSCREEN);
+	INT screen_height = GetSystemMetrics(SM_CYSCREEN);
+
+
+	INT window_width = screen_width * 3 / 4;
+	INT window_height = screen_height * .75;
+	INT window_start_x = screen_width / 8;
+	INT window_start_y = window_height / 8;
+
 	HWND hwnd = CreateWindowEx
 		(
 			NULL,     //ExStyles
 			g_sz_MY_WINDOW_CLASS,  //class name
 			g_sz_MY_WINDOW_CLASS,  //Window title
 			WS_OVERLAPPEDWINDOW,   //windown style
-			CW_USEDEFAULT, CW_USEDEFAULT,    //Window position
-			CW_USEDEFAULT, CW_USEDEFAULT,//window size
+			window_start_x, window_start_y,    //Window position
+			window_width, window_height,//window size
 			NULL,  //Parent Window
 			NULL,  ///Main menu ResourseID for MainWindow or ResourceID for childWindow
 			hInstance,
@@ -67,6 +78,30 @@ INT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 	{
 	case WM_CREATE:
 			break;
+	case WM_MOVE:
+	case WM_SIZE:
+	{
+		RECT rect;
+		GetWindowRect(hwnd, &rect);
+		INT window_width = rect.right - rect.left;
+		INT window_height = rect.bottom - rect.top;
+		CONST INT SIZE = 256;
+		CHAR sz_title[SIZE]{};
+		sprintf(sz_title, "%s - Position:%ix%i;\tSize: %ix%i", g_sz_MY_WINDOW_CLASS, rect.left, rect.top,
+			window_width, window_height);
+		//Функция sprintf(dst,src, parameters) форматирует src-строку , и результат форматирования сохраняет в другую строку
+		// dst-строка получатель 
+		//src- Sourse (строка источник) .Как правило является форматированной строкой.
+		// Которые подменяются соответствующими параметрамию
+		// %i - На место этого спецификатора встовляются значения типа 'int'
+		// %s - на место этого спецификатора вставляется значения типа 'char'
+		// ...f-format
+		//s... -string
+		// printf()- print formated string (Вывести на экран отформатированную строку )
+		SendMessage(hwnd, WM_SETTEXT, 0, (LPARAM)sz_title);
+	}
+
+		break;
 	case WM_COMMAND:
 		break;
 	case WM_DESTROY:
