7a671f213a7e93f56e99bfab4f2cc0d19d63a467
Александр Пичугин <aleksandrpicugin700@gmail.com>
2024-11-19 19:44:51 +0300

    Keyboard almost DONE

--- Calc/main.cpp
+++ Calc/main.cpp
@@ -1,5 +1,6 @@
 ﻿#define _CRT_SECURE_NO_WARNINGS
 #include<Windows.h>
+#include<iostream>
 #include"resource.h"
 
 CONST CHAR g_sz_CLASS_NAME[] = "Calc PV_319";
@@ -85,10 +86,14 @@ INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, IN
 
 INT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
+
     switch (uMsg)
     {
     case WM_CREATE:
     {
+        AllocConsole();
+        freopen("CONOUT$", "w", stdout);
+
         HWND hEdit = CreateWindowEx
         (
             NULL,"Edit","0",
@@ -205,7 +210,7 @@ INT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
         CHAR sz_display[SIZE]{};
         CHAR sz_digit[2] = {};
         HWND hEditDisplay = GetDlgItem(hwnd, IDC_EDIT_DISPLAY);
-            SendMessage(hEditDisplay, WM_GETTEXT, SIZE, (LPARAM)sz_display);
+        SendMessage(hEditDisplay, WM_GETTEXT, SIZE, (LPARAM)sz_display);
         if (LOWORD(wParam) >= IDC_BUTTON_0 && LOWORD(wParam) <= IDC_BUTTON_9)
         {
             if (LOWORD(wParam) == IDC_BUTTON_POINT)
@@ -214,20 +219,45 @@ INT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
                 strcat(sz_display, ".");
             }
             sz_digit[0] = LOWORD(wParam) - IDC_BUTTON_0 + '0';
-           if (strlen(sz_display) == 1 && sz_display[0]== '0')
-            sz_display[0] = sz_digit[0];
-           else
-               strcat(sz_display, sz_digit);
+            if (strlen(sz_display) == 1 && sz_display[0] == '0')
+                sz_display[0] = sz_digit[0];
+            else
+                strcat(sz_display, sz_digit);
             SendMessage(hEditDisplay, WM_SETTEXT, 0, (LPARAM)sz_display);
         }
         if (LOWORD(wParam) == IDC_BUTTON_POINT)
         {
-            if(strchr(sz_display,'.'))break;
+            if (strchr(sz_display, '.'))break;
             strcat(sz_display, ".");
             SendMessage(hEditDisplay, WM_SETTEXT, 0, (LPARAM)sz_display);
         }
+       // if (LOWORD(wParam) == IDC_EDIT_DISPLAY && HIWORD(wParam) == EN_SETFOCUS)
+       //     SetFocus(hwnd);
+        if (LOWORD(wParam) == IDC_BUTTON_BSP)
+        {
+            if (strlen(sz_display) == 1)sz_display[0] = '0';
+            else sz_display[strlen(sz_display) - 1] = 0;
+            SendMessage(hEditDisplay, WM_SETTEXT, 0, (LPARAM)sz_display);
+        }
+        if (LOWORD(wParam) == IDC_BUTTON_CLEAR)
+        {
+                SendMessage(hEditDisplay, WM_SETTEXT, 0, (LPARAM)"0");
+        }
+        SetFocus(hwnd);
     }
         break;
+    case WM_KEYDOWN:
+    {
+        if (wParam >= 0x30 && wParam <= 0x39)
+            SendMessage(hwnd, WM_COMMAND, wParam - 0x30 + IDC_BUTTON_0, 0);
+        switch ( wParam)
+        {
+        case VK_OEM_PERIOD: SendMessage(hwnd, WM_COMMAND, LOWORD(IDC_BUTTON_POINT), 0); break;
+        case VK_ESCAPE: SendMessage(hwnd, WM_COMMAND, LOWORD(IDC_BUTTON_CLEAR), 0); break;
+        case VK_BACK: SendMessage(hwnd, WM_COMMAND, LOWORD(IDC_BUTTON_BSP), 0); break;
+        }
+    }
+    break;
     case WM_DESTROY:
         PostQuitMessage(0);
         break;
